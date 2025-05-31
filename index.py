048184267f1b77b428ca83a0e5b6dcfc7b2f9714
15r <udy15r@gmail.com>
2025-05-31 17:34:13 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,17 @@
-# Setup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("--headless")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-# Visit homepage to set cookies
-driver.get("https://www.nseindia.com")
-# Optional: sleep for 2 seconds
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
-# Set User-Agent (to mimic real browser)
-driver.execute_cdp_cmd('Network.setUserAgentOverride', {
-    "userAgent": "Mozilla/5.0 ..."
-})
-
-# Visit API endpoint
-driver.get("https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20NEXT%2050")
-print(driver.page_source)  # This is the JSON data
+driver.get("https://www.google.com")
+print(driver.title)
 
 driver.quit()
